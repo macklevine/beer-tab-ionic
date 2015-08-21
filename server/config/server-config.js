6758224b78f39c30e46ff3d080c8ee1abdbfff44
macklevine <mackplevine@gmail.com>
2015-08-21 10:18:08 -0700

    did some stuff

--- server/config/server-config.js
+++ server/config/server-config.js
@@ -3,6 +3,9 @@ var handler = require('./server-requests.js');
 var bodyParser = require('body-parser');
 var express = require('express');
 var morgan = require('morgan');
+var path = require('path');
+var fs = require('fs');
+var multer = require('multer');
 
 // Initialize express process
 var app = express();
@@ -28,7 +31,23 @@ app.post('/signup', handler.signupUser);
 app.post('/tabs', handler.toTabs);
 app.post('/location', handler.sendLoc);
 app.post('/profile', handler.profile);
-app.post('/profileupload', handler.upload);
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/www/assets/profiles')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname)
+  }
+});
+
+var upload = multer({ storage: storage });
+
+app.post('/profileupload', upload.single('file'), function(req, res){
+  console.log(req.files); // form files
+  res.status(204).end();
+});
+
 
 // Export server app instance
 module.exports = app;
